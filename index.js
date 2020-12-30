802c06b01951b94830f565d12f3cdacc281ddd84
yagnesh parekh <yagneshparekh@rocketmail.com>
2020-12-30 16:39:36 -0500

    check error

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(cors({
-	origin:"https://book-share-client-yp.herokuapp.com/",
+	origin:"https://book-share-client-yp.herokuapp.com",
 	credentials:true
 }))
 
