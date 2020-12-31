9209cb3179c0ab516e175718d2e43aab84ee7145
yagnesh parekh <yagneshparekh@rocketmail.com>
2020-12-30 20:31:24 -0500

    change samesite and secure

--- middleware/authCheck.js
+++ middleware/authCheck.js
@@ -5,9 +5,8 @@ const createError = require('http-errors')
 
 const authCheck =(req,res, next) =>{
 	//we check the token is exist
-	console.log("req.cookies", req.cookies.jwt)
-	console.log("req.headers", req.headers)
-	console.log("req.headers.cookie", req.headers.cookie)
+	console.log("req.cookies", req.cookies)
+	
 		const token = req.cookies.jwt;
 		if(!token){
 				// return res.status(400).json({message:"Authorization Denied"});
